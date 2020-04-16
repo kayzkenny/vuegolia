0489b7df07f8c6a3771d34bef4028f901b0ca209
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-16 13:48:02 +0100

    build: add new cloud functions

---
+++ functions/seed.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+admin.initializeApp();
+
+const faker = require("faker");
+
+const db = admin.firestore();
+
+const fakeIt = () => {
+  return db.collection("customers").add({
+    username: faker.internet.userName(),
+    email: faker.internet.email(),
+    avatar: faker.internet.avatar(),
+    bio: faker.hacker.phrase(),
+    color: faker.commerce.color(),
+  });
+};
+
+Array(20).fill(0).forEach(fakeIt);
