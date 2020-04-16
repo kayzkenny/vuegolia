614941124f46f2efacd3401a05f3edda4fd95e7f
Kehinde Akeredolu <kayzkenny@gmail.com>
2020-04-16 14:37:48 +0100

    build: add vue-instantsearch examples

--- vuegolia/src/router/index.js
+++ vuegolia/src/router/index.js
@@ -1,29 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Basic from "../views/Basic.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    name: "Basic",
+    component: Basic,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+    path: "/intermediate",
+    name: "Intermediate",
+    component: () => import("../views/Intermediate.vue"),
+  },
+  {
+    path: "/advanced",
+    name: "Advanced",
+    component: () => import("../views/Advanced.vue"),
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ vuegolia/src/views/Intermediate.vue
@@ -0,0 +1,54 @@
+<template>
+  <ais-instant-search index-name="demo_ecommerce" :search-client="searchClient">
+    <div class="left-panel">
+      <ais-clear-refinements />
+      <h2>Colors</h2>
+      <ais-refinement-list attribute="brand" searchable />
+      <ais-configure :hitsPerPage="8" />
+    </div>
+    <div class="right-panel">
+      <ais-search-box />
+      <ais-hits>
+        <div slot="item" slot-scope="{ item }">
+          <h2>{{ item.name }}</h2>
+        </div>
+      </ais-hits>
+      <ais-pagination />
+    </div>
+  </ais-instant-search>
+</template>
+
+<script>
+import algoliasearch from "algoliasearch/lite";
+import "instantsearch.css/themes/algolia-min.css";
+
+export default {
+  name: "Basic",
+  data() {
+    return {
+      searchClient: algoliasearch(
+        "B1G2GM9NG0",
+        "aadef574be1f9252bb48d4ea09b5cfe5"
+      ),
+    };
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+  padding: 1em;
+}
+
+.ais-Hits-list {
+  margin-top: 0;
+  margin-bottom: 1em;
+}
+
+.ais-InstantSearch {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-gap: 1em;
+}
+</style>
